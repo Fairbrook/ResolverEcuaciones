b04eafa8821a90ca4e5c611047abf84c7145f7dd
Fairbrook <32619805+Fairbrook@users.noreply.github.com>
2017-10-08 13:46:40 -0500

    Initial commit

---
+++ ResolverEcuaciones/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ResolverEcuaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(142, 25);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(62, 20);
+            this.txtNum.TabIndex = 0;
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(13, 28);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(123, 13);
+            this.lblNum.TabIndex = 1;
+            this.lblNum.Text = "Numero de ecuaciones: ";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(224, 23);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnOk.Size = new System.Drawing.Size(56, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "Aceptar";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResult.Location = new System.Drawing.Point(12, 247);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 3;
+            this.btnResult.Text = "Calcular";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Visible = false;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(224, 22);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 4;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Visible = false;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(332, 282);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Resolver Ecuaciones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
+
---
+++ ResolverEcuaciones/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpMatrices;
+using OpMatrices.Proceso;
+using OpMatrices.Types;
+
+namespace ResolverEcuaciones
+{
+    public partial class Form1 : Form
+    {
+        int num;
+        TextBox[,] myText;
+        TextBox[] txtResult;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                btnNuevo.Visible = true;
+                num = Convert.ToInt32(txtNum.Text);
+                myText = new TextBox[num, num + 1];
+                txtResult = new TextBox[num];
+                Label mylabel;
+                for (int i = 0; i < num; i++)
+                {
+                    mylabel = new Label();
+                    mylabel.Location = new Point(5, 80 + (i * 27));
+                    mylabel.Text = "E" + (i + 1);
+                    mylabel.Size = new System.Drawing.Size(20, 15);
+                    this.Controls.Add(mylabel);
+
+                    for (int j = 0; j < num; j++)
+                    {
+                        if (i == 0)
+                        {
+                            mylabel = new Label();
+                            mylabel.Location = new Point(38 + (j * 60), 65 + (i * 25));
+                            mylabel.Size = new System.Drawing.Size(20, 15);
+                            mylabel.Text = "X" + (j + 1);
+                            this.Controls.Add(mylabel);
+                        }
+                        mylabel = new Label();
+                        myText[i, j] = new TextBox();
+
+                        myText[i, j].Location = new Point(25 + (j * 60), 80 + (i * 25));
+                        myText[i, j].Name = "txtDat" + i;
+                        myText[i, j].Size = new System.Drawing.Size(50, 20);
+
+                        mylabel.Location = new Point(75 + (j * 60), 80 + (i * 25));
+                        if (j == num - 1) mylabel.Text = "=";
+                        else mylabel.Text = "+";
+                        mylabel.Size = new System.Drawing.Size(10, 10);
+
+                        this.Controls.Add(mylabel);
+                        this.Controls.Add(myText[i, j]);
+                    }
+                }
+                mylabel = new Label();
+                mylabel.Location = new Point(38 + (num * 60), 65);
+                mylabel.Size = new System.Drawing.Size(20, 15);
+                mylabel.Text = "R";
+                this.Controls.Add(mylabel);
+
+                for (int i = 0; i < num; i++)
+                {
+                    mylabel = new Label();
+                    txtResult[i] = new TextBox();
+                    txtResult[i].Location = new Point(25 + (num * 60), 80 + (i * 25));
+                    txtResult[i].Name = "txtDat" + i;
+                    txtResult[i].Size = new System.Drawing.Size(50, 20);
+
+                    mylabel.Location = new Point(75 + (num * 60), 80 + (i * 25));
+                    mylabel.Text = " ";
+                    mylabel.Size = new System.Drawing.Size(10, 10);
+
+                    this.Controls.Add(mylabel);
+                    this.Controls.Add(txtResult[i]);
+
+                }
+                txtNum.Enabled = false;
+                btnResult.Visible = true;
+                btnOk.Visible = false;
+                btnResult.Location = new Point(25, 80 + (num * 25));
+            }
+            catch (Exception) { MessageBox.Show("Cantidad inválida"); }
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            Label lblRs;
+            Fraccion[,] frac = new Fraccion[num, num];
+            double[,] array = new double[num,1];
+            Matriz<Fraccion> MatrizA = new Matriz<Fraccion>();
+            Matriz<double> MatrizB = new Matriz<double>();
+            Matriz<double> MatrizAP = new Matriz<double>();
+            try
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    for (int j = 0; j < num; j++)
+                    {
+                        frac[i, j] = new Fraccion();
+                        frac[i, j].setFrac(Convert.ToInt32(myText[i, j].Text), 1);
+                    }
+                }
+                MatrizA.setMatriz(num, num, frac);
+                for (int i = 0; i < num; i++)
+                {
+                    array[i, 0] = Convert.ToDouble(txtResult[i].Text);
+                }
+                MatrizB.setMatriz(num, 1, array);
+                MatrizA = Operaciones.inver(MatrizA);
+                MatrizAP = Operaciones.MtrzFracToDouble(MatrizA);
+                MatrizB = Operaciones.mult(MatrizAP, MatrizB);
+                array = MatrizB.getMatriz();
+                for (int i = 0; i < num; i++)
+                {
+                    lblRs = new Label();
+                    lblRs.Location = new Point(25, 80 + ((num+1+i) * 25));
+                    lblRs.Text = "X" + (i + 1) + ": " + array[i,0];
+                    lblRs.Size = new System.Drawing.Size(50, 20);
+                    this.Controls.Add(lblRs);
+                }
+
+                btnResult.Location = new Point(25, 80 + (num * 25));
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Cantidad Inválida");
+            }
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            Form1 formulario = new Form1();
+            formulario.Show();
+            this.Dispose(false);
+        }
+    }
+}
